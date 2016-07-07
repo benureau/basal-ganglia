Correct Hebbian formula.

experiments/model.py
@@ -231,6 +231,6 @@ class Model(object):
             cue = np.argmax(self["CTX"]["cog"]["U"])
 
             LTP   = _["Hebbian"]["LTP"]
-            dw = LTP * self["CTX"]["cog"]["U"][cue]
+            dw = LTP * self["CTX"]["cog"]["U"][cue] * self["CTX"]["ass"]["U"][cue]
             W = self["CTX:cog → CTX:ass"].weights
             W[cue] += dw * (Wmax - W[cue]) * (W[cue] - Wmin)