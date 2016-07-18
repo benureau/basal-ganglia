fb316a2b9c3a20a2c72a094e16371af2977b4b53
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2016-07-18 23:39:08 +0200

    Graphs notebook code

---
+++ experiments/single_stimulus/graphs.py
@@ -0,0 +1,58 @@
+import numpy as np
+import colorsys
+
+from bokeh import io
+from bokeh import plotting as bkp
+from bokeh.plotting import show
+from bokeh.core.properties import value
+from bokeh.models import FixedTicker
+from bokeh.models.mappers import LinearColorMapper
+
+from ipywidgets.widgets import interact, fixed, IntSlider, FloatSlider, SelectionSlider
+
+
+    ## Disable autoscrolling
+
+from IPython.display import display, Javascript
+
+disable_js = """
+IPython.OutputArea.prototype._should_scroll = function(lines) {
+    return false;
+}
+"""
+display(Javascript(disable_js))
+
+
+    ## Load bokeh for jupyter
+
+bkp.output_notebook(hide_banner=True)
+
+
+    ## Better default figures
+
+def tweak_fig(fig):
+    tight_layout(fig)
+    disable_minor_ticks(fig)
+    disable_grid(fig)
+    fig.toolbar.logo = None
+
+def tight_layout(fig):
+    fig.min_border_top    = 35
+    fig.min_border_bottom = 35
+    fig.min_border_right  = 35
+    fig.min_border_left   = 35
+
+def disable_minor_ticks(fig):
+    #fig.axis.major_label_text_font_size = value('8pt')
+    fig.axis.minor_tick_line_color = None
+    fig.axis.major_tick_in = 0
+
+def disable_grid(fig):
+    fig.xgrid.grid_line_color = None
+    fig.ygrid.grid_line_color = None
+
+
+def figure(*args, **kwargs):
+    fig = bkp.figure(*args, **kwargs)
+    tweak_fig(fig)
+    return fig
