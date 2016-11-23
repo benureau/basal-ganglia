9b301960ae993a4ffa2c4811277db7e18f02c82c
Fabien C. Y. Benureau <fabien.benureau+git@gmail.com>
2016-11-23 07:29:49 +0100

    Don't push trace file to the cluster

--- experiments/one_stimulus/cluster/push.sh
+++ experiments/one_stimulus/cluster/push.sh
@@ -8,6 +8,8 @@ rsync -azv --delete \
       --exclude *.npy \
       --exclude *.pyc \
       --exclude *.egg-info \
+      --exclude *.trace \
       --exclude .git/ \
+      --exclude data/ \
       --exclude __pycache__/ \
-      ../.. avakas:monkeys/
+      ../../.. avakas:monkeys/
